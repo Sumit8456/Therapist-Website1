57c4048958f89e4b96a904a20cac765d356f87cf
Sumit Lakkavatri <156050570+Sumit8456@users.noreply.github.com>
2025-07-06 13:26:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import OperationDashboard from './pages/OperationDashboard';
+
 import Page from './app/page';
 import About from './components/About';
 import Services from './components/Services';
@@ -18,10 +16,7 @@ const App = () => {
         <Route path="/services" element={<Services/>} />
          <Route path="/faq" element={<FAQ/>} />
          <Route path="/contact" element={<ContactForm/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/operation" element={<OperationDashboard />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
+     
       </Routes>
     </Router>
   );
